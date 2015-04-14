545ba07657e30df0afd9ad9585d9d2868add0db8
jbzdarkid <jbzdarkid@gmail.com>
2015-04-14 10:44:54 -0400

    Adding IconEnum, which I forgot.

---
+++ src/main/java/gui/IconEnum.java
@@ -0,0 +1,17 @@
+package gui;
+
+public enum IconEnum {
+    None,
+    Mod,
+    Broadcaster,
+    Admin,
+    Staff,
+    Turbo,
+    Subscriber,
+    Donator_basic,
+    Donator_low,
+    Donator_medium,
+    Donator_high,
+    Donator_insane,
+    GlobalMod
+}
